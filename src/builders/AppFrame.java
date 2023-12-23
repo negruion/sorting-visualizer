71fc781bdb4d03d77dbeb421c2a15aa43fbf2620
NegruIon <ionegru776@gmail.com>
2023-12-23 20:32:12 +0100

    create app GUI frame

--- src/builders/AppFrame.java
+++ src/builders/AppFrame.java
@@ -8,6 +8,16 @@ import javax.swing.*;
 public class AppFrame extends JFrame{
 
     public AppFrame(){
+        this.setTitle("AlgoVisualizer");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setSize(1200, 700);
+        this.setLayout(null);
+
+
+        this.requestFocusInWindow();
+        this.repaint();
+        this.setVisible(true);
 
 
     }
