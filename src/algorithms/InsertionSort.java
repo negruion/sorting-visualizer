fe6e2d9f50f00f7e55f2d52aec4437605fe0c825
NegruIon <ionegru776@gmail.com>
2024-01-04 17:35:33 +0100

    implement the visualizer with green and red bars

--- src/algorithms/InsertionSort.java
+++ src/algorithms/InsertionSort.java
@@ -12,49 +12,68 @@ public class InsertionSort {
 
     BarPanel barPanel;
 
-    public InsertionSort(BarPanel barPanel){
+    public InsertionSort(BarPanel barPanel) {
         this.barPanel = barPanel;
     }
 
     /**
-     * Sorts the bars using the insertion sort algorithm.
+     * Main method for sorting the bars.
+     * @param speed the sorting speed
      */
     public void sort(int speed) {
         List<Rectangle> bars = barPanel.getBars();
 
-        Timer timer = new Timer(speed*10, null);
+        SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+            @Override
+            protected Void doInBackground() throws Exception {
+                for (int i = 1; i < bars.size(); i++) {
+                    Rectangle key = bars.get(i);
+                    int j = i - 1;
 
-        int[] i = { 1 };
+                    barPanel.setGreenBar(i);
+                    barPanel.repaint();
 
-        timer.addActionListener(e -> {
-            if (i[0] < bars.size()) {
-                Rectangle key = bars.get(i[0]);
-                int j = i[0] - 1;
+                    try {
+                        Thread.sleep(speed);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
 
-                while (j >= 0 && bars.get(j).getHeight() > key.getHeight()) {
-                    Rectangle currentBar = bars.get(j);
-                    Rectangle nextBar = bars.get(j + 1);
+                    while (j >= 0 && bars.get(j).getHeight() > key.getHeight()) {
+                        Rectangle currentBar = bars.get(j);
+                        Rectangle nextBar = bars.get(j + 1);
 
-                    int x_1 = currentBar.x, y_1 = currentBar.y, x_2 = nextBar.x, y_2 = nextBar.y;
-                    currentBar.setLocation(x_2, y_1);
-                    nextBar.setLocation(x_1, y_2);
+                        barPanel.setRedBar(j);
+                        barPanel.repaint();
 
-                    bars.set(j, nextBar);
-                    bars.set(j + 1, currentBar);
+                        int x_1 = currentBar.x, y_1 = currentBar.y, x_2 = nextBar.x, y_2 = nextBar.y;
+                        currentBar.setLocation(x_2, y_1);
+                        nextBar.setLocation(x_1, y_2);
 
-                    j--;
+                        bars.set(j, nextBar);
+                        bars.set(j + 1, currentBar);
+
+                        j--;
+
+                        SwingUtilities.invokeLater(() -> barPanel.repaint());
+
+                        try {
+                            Thread.sleep(speed);
+                        } catch (InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+
+                    bars.set(j + 1, key);
                 }
 
-                bars.set(j + 1, key);
-                i[0]++;
-
+                barPanel.barColourReset();
                 barPanel.repaint();
-            } else {
-                timer.stop();
+
+                return null;
             }
-        });
+        };
 
-        timer.start();
+        worker.execute();
     }
-
 }
